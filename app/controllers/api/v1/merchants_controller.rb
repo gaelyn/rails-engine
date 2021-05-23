7708ea3b6f09f7498f03d7a1f3b70a05fa203c2f
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-23 12:39:06 -0500

    Implement limit for merchants per page

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.all)
+    @merchants = Merchant.limit(per_page).offset(params[:offset])
+    render json: MerchantSerializer.new(@merchants)
   end
 
   private
+
+  def per_page
+    params.fetch(:per_page, 20).to_i
+  end
 
   def merchant_params
     params.require(:merhcant).permit(:name)
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,15 +10,63 @@ describe "Merchants API", type: :request do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(3)
-    
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+    expect(merchants[:data].count).to eq(3)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
-
+    merchants[:data].each do |merchant|
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can return an array of data even if zero resources found' do
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(0)
+    expect(merchants[:data]).to be_an(Array)
+  end
+
+  it "returns a subset of merchants based on per page limit" do
+    create_list(:merchant, 40)
+
+    get '/api/v1/merchants', params: { per_page: 20 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(20)
+  end
+
+  it "returns a subset of merchants based on limit and offset" do
+    create_list(:merchant, 30)
+
+    get '/api/v1/merchants', params: { per_page: 20, offset: 20 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(10)
+
+    expect(merchants[:data].first[:id].to_i).to eq(Merchant.all[20].id)
+    expect(merchants[:data].first[:attributes][:name]).to eq(Merchant.all[20].name)
+  end
+
+  it "has a limit of 20 results per page if no limit given" do
+    create_list(:merchant, 30)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(20)
+    expect(merchants[:data].first[:attributes][:name]).to eq(Merchant.first.name)
+    expect(merchants[:data].last[:attributes][:name]).to eq(Merchant.all[19].name)
   end
 end
