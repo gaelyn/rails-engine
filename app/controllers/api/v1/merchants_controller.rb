0d88816b04a3c3f417f61fbc089126398436c93d
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-23 16:43:54 -0500

    Refactor page and per_page methods into application_controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,15 +3,4 @@ class Api::V1::MerchantsController < ApplicationController
     @merchants = Merchant.limit(per_page).offset(page)
     render json: MerchantSerializer.new(@merchants)
   end
-
-  private
-
-  def per_page
-    params.fetch(:per_page, 20).to_i
-  end
-
-  def page
-    page = [params.fetch(:page, 1).to_i, 1].max
-    (page - 1) * per_page
-  end
 end
