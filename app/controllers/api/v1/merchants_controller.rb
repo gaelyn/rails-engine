00d01480184231f69e2b28ddf3b3fe2f1d5cf36d
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-22 19:13:33 -0500

    Update Gemfile to include fast_jsonapi and create merchant serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  private
+
+  def merchant_params
+    params.require(:merhcant).permit(:name)
+  end
 end
