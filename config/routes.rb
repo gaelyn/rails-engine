e8d4f3b8bf7ee29a892390f334c3b0a904b05dd9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-25 17:13:35 -0500

    Get all items for one merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants#find'
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show], controller: 'merchants' do
+        resources :items, only: [:index]
+      end
       get '/items/find_all', to: 'items#find_all'
       resources :items, only: [:index, :show]
     end
