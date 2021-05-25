fe658a9603853f77a55288be043cfb74ce67c548
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-25 17:43:14 -0500

    Find merchant for a given item id

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -119,4 +119,22 @@ describe "Items API", type: :request do
     items = JSON.parse(response.body, symbolize_names: true)
     expect(items[:data].count).to eq(2)
   end
+
+  it "can get the merchant for a given item ID" do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item1 = create(:item, merchant: merchant1)
+    item2 = create(:item, merchant: merchant1)
+    item3 = create(:item, merchant: merchant1)
+    item4 = create(:item, merchant: merchant2)
+
+    get "/api/v1/items/#{item4.id}/merchant"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(merchant[:data]).to be_a Hash
+
+    expect(merchant[:data][:id].to_i).to eq(merchant2.id)
+  end
 end
