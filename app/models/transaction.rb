3d6689296d525356f7975f8c2d500db272cfe59c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-26 22:45:42 -0500

    Verified and refactored relationships and added most_revenue to merchant

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
   has_many :invoice_items, through: :invoice
+  has_many :items, through: :invoice_items
+  has_one :merchant, through: :invoice
 
   validates_presence_of :invoice_id, :credit_card_number, :result
 end
